3aeb5b4b09b14ca1722780240fb7e6e40ad54145
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-01-09 12:06:49 -0300

    arquivo check.php adicionado

--- Game/check.php
+++ Game/check.php
@@ -21,4 +21,15 @@
   "home.php?user=" . $user
 
   */
+session_start();
+$_SESSION['jogadores'] = $_GET['jogadores'];
+require_once('funcaoCheck.php');
+
+if (analise($_SESSION['jogadores'])) {
+    header('Location: /Game/coluna.php');
+    exit;
+} else {
+    header('Location: /Game/config.php?jogadores=' . $_GET['jogadores']);
+    exit;
+}
 ?>
