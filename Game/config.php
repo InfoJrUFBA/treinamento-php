702546a6a9780d355da1ea16e5df7d132053dd69
Luan Lemos Almeida <34046499+kylb@users.noreply.github.com>
2018-01-08 23:25:46 -0300

    Criação
    Criação de acordo com requisitos solicitados.

--- Game/config.php
+++ Game/config.php
@@ -1,27 +1,33 @@
-<?php
-/*
-1 - Crie um Formulário que Envie Dados para check.php
-
-2 - O dado passado deve ser: "jogadores"
-
-Dica:
-
-  Lembrem-se que o que define a chave da variavél que será passada para a página é o atributo "name" no input. Observe:
-
-  <input type="text" name="nomeDaVariavel">
-
-3 - A variavél "jogadores", representa o Numero de Jogadores, logo a <label> desse <input> deve conter essa informação.
-
-4 - Os inputs devem utilizar o atributo "value", sendo igual ao valor da variavel $_GET['nomeDaVariavel']. Isso será bastante
-útil se a página check.php fazer um redicrionamento de volta para config.php.
-
-  Dica: Veja como vocês podem utilizar o atributo "value" da tag <input>:
-    Nome: <input type="text" name="fname" value="John">
-
-  Mas se o Nome estivesse em alguma variavél?
-    Nome: <input type="text" name="name" value=<?php echo $minhaVariavel; ?> >
-
-  Mais detalhes em: https://www.w3schools.com/tags/att_input_value.asp
-
-  */
-?>
+<!DOCTYPE html>
+<html lang="pt">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Game PHP</title>
+		<meta name="description" content="">
+		<meta name="author" content="luanlemos">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	</head>
+	<body>
+		<div>
+			<header>
+				<h1>InfoJrUFBA - Treinamento PHP</h1>
+			</header>
+			<nav>
+				<p></p>
+			</nav>
+			<div>
+				<form action="check.php" method="get" id="formPlayersNumber">
+				  <label for="players">Numero de Jogadores:</label>
+				  <input type="text" name="players" id="players" value="<?php echo $_GET['players']; ?>"><br />
+				  <input type="submit" value="Enviar">
+				</form>
+			</div>
+			<footer>
+				<p>
+					CopyLeft: lemosluan@infojr.com.br
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
