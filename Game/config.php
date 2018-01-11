669012ab0b39d1fbd81f2803f6e26ec535f6f8ae
Iris Nogueira <irisnogueira@infojr.com.br>
2018-01-11 11:58:19 -0300

    Etapas 5 e 6 finalizadas. Na 5ª, a página chama a função coluna e recebe as pontuações; na 6ª a página exibe o resultado

--- Game/config.php
+++ Game/config.php
@@ -1,5 +1,5 @@
 <form method="get" action="/Game/check.php">
     <p>Insira a quantidade de jogadores: <br>
-    <input type="number" name="jogadores" min="1" max="50" value=<?php echo $_GET['jogadores']; ?>> <br>
+    <input type="number" name="jogadores" min="2" max="50" value=<?php echo $_GET['jogadores']; ?>> <br>
     <input type="submit" name="Enviar">
 </form>
