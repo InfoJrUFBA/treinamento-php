65c25c8ba4c6a3e1bc37413052e1f4fd606dc2ec
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-01-11 10:43:46 -0300

    codigo melhor para finalizacao do arquivo, tirando o laço 'for' e usando apenas uma variavel

--- Game/resultado.php
+++ Game/resultado.php
@@ -8,17 +8,9 @@
     <body>
         <?php
         session_start();
-        $pts = array_rand($_POST);
-        $jog = 0;
+        $jog = array_rand($_POST);
 
-        for($i=1; $i<=$_SESSION["jogadores"]; $i++) {
-            if ($_POST[$i] == $_POST[$pts]) {
-                $jog = $i;
-                break;
-            }
-        }
-
-        echo "<h1>O Jogador " . $jog . " foi o Vencedor com " . $_POST[$pts] . " pontos</h1>";
+        echo "<h1>O Jogador " . $jog . " foi o Vencedor com " . $_POST[$jog] . " pontos</h1>";
         ?>
     </body>
 </html>
