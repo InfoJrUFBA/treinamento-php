826fcb82c6bf71af71274deec2741e75571fe535
Valdinei Ferreira <valdinei_vof@hotmail.com>
2018-01-11 10:04:13 -0300

    ajustes finais

--- Game/resultado.php
+++ Game/resultado.php
@@ -1,16 +1,24 @@
-<?php
-session_start();
-$pts = array_rand($_POST);
-$jog = 0;
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Resultado</title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        $pts = array_rand($_POST);
+        $jog = 0;
 
-for($i=1; $i<=$_SESSION["jogadores"]; $i++) {
-  if ($_POST[$i] == $_POST[$pts]) {
-    $jog = $i;
-    break;
-  }
-}
+        for($i=1; $i<=$_SESSION["jogadores"]; $i++) {
+            if ($_POST[$i] == $_POST[$pts]) {
+                $jog = $i;
+                break;
+            }
+        }
 
-var_dump($_POST);
-
-echo "<h1>O Jogador " . $jog . " foi o Vencedor com " . $_POST[$pts] . " pontos</h1>";
-?>
+        echo "<h1>O Jogador " . $jog . " foi o Vencedor com " . $_POST[$pts] . " pontos</h1>";
+        ?>
+    </body>
+</html>
